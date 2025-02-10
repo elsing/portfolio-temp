7714b8597e9fb3a1aaa2dc51d3ee6d5e35c89b47
elsing <elliot@singermail.uk>
2025-02-10 22:13:23 +0000

    fix unescaped entity

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -4,7 +4,7 @@ export default function SocialIcons() {
   return (
     <div>
       <p className="flex justify-center py-2">
-        Don't be a stranger, feel free to reach out to me!
+        Don&apos;t be a stranger, feel free to reach out to me!
       </p>
     <div className="flex gap-2 pb-4 justify-center">
       <SocialIcon url="https://www.linkedin.com/in/elliotsinger/" />
